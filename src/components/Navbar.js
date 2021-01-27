69470f6292663fe0295d2505c9a0b45f2997fc2c
orlandCasta <orlandos.casta@gmail.com>
2021-01-27 16:43:29 -0600

    add change in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends React.Component{
         return(
             <div className="Navbar">
                 <div className="Navbar__brand">
-                    <Link clasName="Navbar_brand-logo" to="/">
+                    <Link clasName="Navbar_brand-logo" to="/React-JS-Application">
                         <img className="Navbar__brand-logo" src={logo} alt="Logo"/>
                         <span className="font-weight-light">Platzi</span>
                         <span className="font-weight-bold">Conf</span>
